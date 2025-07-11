558135dd09c56ce7b6818e27ad74d3f0841d50ee
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2025-07-11 20:29:58 +0900

    add geojson

---
+++ geojson/script.js
@@ -0,0 +1,20 @@
+// Initialize the map
+const map = L.map('map').setView([35.681236, 139.767125], 13); // Centered on Tokyo Station
+
+// Add a tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Load and display GeoJSON data
+fetch('data.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            onEachFeature: function (feature, layer) {
+                if (feature.properties && feature.properties.name) {
+                    layer.bindPopup(feature.properties.name);
+                }
+            }
+        }).addTo(map);
+    });
